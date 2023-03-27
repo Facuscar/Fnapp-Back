1b182907899d178e933d26359bb6b40edc0e1904
Facuscar <facuscardillo@gmail.com>
2023-03-27 18:57:28 -0300

    Create all user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,15 @@
-import express from 'express';
+import express from "express"
+import { registerUser, authUser, confirm, forgotPassword, verifyToken, newPassword, profile } from "../controllers/userController.js";
+import checkAuth from "../middleware/checkAuth.js";
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', (req, res) => {
-   res.send('From the API');
-});
+router.post('/', registerUser);
+router.post('/login', authUser);
+router.get('/confirm/:token', confirm);
+router.post('/forgot-password', forgotPassword);
+router.route('/forgot-password/:token').get(verifyToken).post(newPassword);
+
+router.get('/profile', checkAuth, profile)
 
 export default router;
