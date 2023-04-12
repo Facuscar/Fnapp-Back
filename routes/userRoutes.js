999ef478da0360aa123d7dd219fff864123e8bd6
Facuscar <facuscardillo@gmail.com>
2023-04-11 21:53:22 -0300

    Fix method in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { registerUser, authUser, confirm, forgotPassword, verifyToken, newPassword, profile } from "../controllers/userController.js";
+import { receiveEmail, authUser, confirm, forgotPassword, verifyToken, newPassword, profile } from "../controllers/userController.js";
 import checkAuth from "../middleware/checkAuth.js";
 
 const router = express.Router()
 
-router.post('/', registerUser);
+router.post('/', receiveEmail);
 router.post('/login', authUser);
 router.get('/confirm/:token', confirm);
 router.post('/forgot-password', forgotPassword);
